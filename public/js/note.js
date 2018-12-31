5953d6ba1f51ed263521ae7f1f935cdbeb8eed0f
jhplus38 <jhplus38@naver.com>
2018-12-31 10:50:08 +0900

    첫번째 커밋

---
+++ public/js/note.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyDrKA8uyEdu6K2q6fvob2gjb7D_8fa0l1c",
+	authDomain: "jhplus38-note.firebaseapp.com",
+	databaseURL: "https://jhplus38-note.firebaseio.com",
+	projectId: "jhplus38-note",
+	storageBucket: "jhplus38-note.appspot.com",
+	messagingSenderId: "850236210453"
+  };
+  firebase.initializeApp(config);
